79889e9c325383dfb2cb3f6e24229626e65ce341
Aditya Kumar <aadikori9085@gmail.com>
2025-05-15 13:07:06 +0530

    Update SearchBar.js
    Update the comments

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 
 function SearchBar({onSubmit}) {
+  //create state for getting value from input
   const [search,setSearch] = useState('')
+  //child to parent using callback function
   const handleClick=(event)=>{
     event.preventDefault()
     onSubmit(search)
     console.log('new search is',search)
     
   }
-
+//change the state
   const handleChanges=(event)=>{
     setSearch(event.target.value)
   }
   
-  
+  //UI for input
   return <div>
     <form onSubmit={handleClick}>
     <input type="text" placeholder="Search for Images" onChange={handleChanges} />

0f97cecdddb3bec9beaa4c08e09431f03c3df243
Aditya Kumar <aadi.kori.9085@gmail.com>
2025-05-16 12:25:00 +0530

    In pics , handling the input using react and also use hooks call useState

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import searchImage from './api'
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-searchImage()
+
 root.render(<App />);
 
 // 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y

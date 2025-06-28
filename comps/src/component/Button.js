09eaeb57deb25ce7f56a25146123a62a65546a58
Aditya Kumar <aadikori9085@gmail.com>
2025-06-28 20:20:37 +0530

    Clean branch for new project

---
+++ comps/src/component/Button.js
@@ -0,0 +1,25 @@
+function Button({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) {
+    return <button>{children}</button>
+}
+Button.propTypes = {
+    checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+        const count = !!Number(primary)
+            + Number(!!secondary)
+            + Number(!!warning)
+            + Number(!!success)
+            + Number(!!danger)
+        if (count > 1) {
+            throw new Error('Only one of primary, secondary , success , warning , danger use at a time')
+        }
+    }
+}
+export default Button;
--- src/components/ImageShow.js
+++
@@ -1,8 +0,0 @@
-function ImageShow({image}) {
-  //get the images as props 
-  //and set to img to show in browser
-  return <div><img src={image.urls.small
-  }/></div>;
-}
-
-export default ImageShow;

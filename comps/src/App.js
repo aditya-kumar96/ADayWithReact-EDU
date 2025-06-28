eb841cb42ffbdb73973c36d1b7e4f9ff4e9d8cd2
Aditya Kumar <aadikori9085@gmail.com>
2025-06-28 20:20:37 +0530

    Clean branch for new project

---
+++ comps/src/App.js
@@ -0,0 +1,23 @@
+import Button from "./component/Button";
+
+function App(){
+    return <div>
+
+        <div>
+            <Button primary secondary>Click Me </Button>
+        </div>
+        <div>
+            <Button>Buy Now </Button>
+        </div>
+        <div>
+            <Button>See Deal </Button>
+        </div>
+        <div>
+            <Button>Hide Ads </Button>
+        </div>
+        <div>
+            <Button>Something </Button>
+        </div>
+    </div>
+}
+export default App;

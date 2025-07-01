f8e2c6c9c50f78756597e5ae059465de064f7b36
Aditya Kumar <aadikori9085@gmail.com>
2025-07-02 01:44:29 +0530

    Added Dropdown Menu

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,26 @@
+
+import Accordion from "../component/Accordion";
+import Button from "./component/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go'
+
+function AccordionPage() {
+    const items= [
+        {
+            id:'1dd',
+            label: 'Home',
+            content: 'Here is the Home'
+        },
+        {
+            id:'23',
+            label: 'Menu',
+            content: 'Here is the Menu'
+        },
+        {
+            id:'dfjh',
+            label: 'Settings',
+            content: 'Here is the Settings'
+        },
+    ]
+    return <Accordion items={items} />
+}
+export default AccordionPage;

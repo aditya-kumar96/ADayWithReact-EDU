020d04cbc205aff23397b23fb177aecd4ba142b9
Aditya Kumar <aadikori9085@gmail.com>
2025-07-03 07:30:03 +0530

    Updated

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -9,7 +9,13 @@ function Dropdown({ options, value, onChange }) {
 
     const divEl = useRef()
     useEffect(()=>{
+        
         const handler=(event)=>{
+            if(!divEl.current){
+                return;
+            }
+
+
             if(!divEl.current.contains(event.target)){
                 setIsSelected(false)
             }

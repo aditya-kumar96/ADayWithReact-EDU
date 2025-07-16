3916e04f93c538db19431dffb3392d041fe36cfa
Aditya Kumar <aadikori9085@gmail.com>
2025-07-16 12:55:09 +0530

    udpates

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { usersReducer } from './slices/usersSlice';
+import { albumApi, useFetchAlbumsQuery } from './api/albumApi';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumApi.reducerPath] : albumApi.reducer
   },
+  middleware:(getDefaultMiddleware)=>{
+    return getDefaultMiddleware()
+    .concat(albumApi.middleware)
+  }
 });
 
+setupListeners(store.dispatch)
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
+
+export {useFetchAlbumsQuery} from './api/albumApi'
